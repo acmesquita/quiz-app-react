f45a110a6e172b755c575e9cd02465bd3ae56ef0
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-21 19:01:29 -0300

    Criando questions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Card } from './styles';
 
 export default function Home() {
+  const [userName, setUserName] = useState('')
+
   return (
     <Container>
       <Card>
         <h2>Quiz App</h2>
         <input
           placeholder='Your Name'
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
         />
-        <button>Start Quiz</button>
+        {userName && <Link to='/questions' className='btn-default'>Start Quiz</Link>}
       </Card>
     </Container>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Questions from './pages/Questions';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/questions' exact component={Questions} />
       </Switch>
     </BrowserRouter>
   )
