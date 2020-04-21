8494cf5eb86cd4e20548953cb10e942cf617e147
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-21 17:46:42 -0300

    Criando componente home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <h1>Hello Quiz</h1>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Home() {
+  return (
+    <>
+      <input
+        placeholder='Your Name'
+      />
+      <button>Start Quiz</button>
+    </>
+  );
+}
